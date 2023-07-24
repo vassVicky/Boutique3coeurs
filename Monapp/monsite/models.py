9f8e155968ec7200a9ee46f4dde1079cbad37df6
Sédrick Gael KOUAGNI <kanangamansedrickgael@gmail.com>
2023-07-24 00:49:31 +0000

    Panier

--- Monapp/monsite/models.py
+++ Monapp/monsite/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
-
+from django.contrib.sessions.models import Session
 
 
 
@@ -39,6 +39,38 @@ class Livraison(models.Model):
         return self.ville
 
 
+class Produit(models.Model):
+    designation_produit = models.CharField(max_length=255)
+    prix_produit = models.DecimalField(max_digits=10, decimal_places=2)
+    details_produit = models.TextField()
+    image_produit = models.ImageField(upload_to='produits/')
+    qte_produit = models.PositiveIntegerField()
+    categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE)
+
+
+    def _str_(self):
+        return self.designation_produit
+
+
+class Panier(models.Model):
+
+    date_create = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    statut_panier = models.BooleanField(default=True) 
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, null=True)
+    session = models.ForeignKey(Session, on_delete=models.CASCADE, null=True)
+
+    @property
+    def prix_total(self):
+        produits = ProduitPanier.objects.filter(panier__id=self.id)
+        somme = 0
+        for produit in produits:
+            somme += produit.prix_total
+        return somme
+    
+    def _str_(self):
+        return str(self.statut_commande)
+
 
 
 class Commande(models.Model):
@@ -51,30 +83,64 @@ class Commande(models.Model):
     date_commande = models.DateTimeField()
     qte_commande = models.PositiveIntegerField()
     statut_commande = models.CharField(max_length=255 , choices=STATUS_COMMANDE) 
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, null=True)
+    nom_client = models.CharField(max_length=255)
+    prenoms_client = models.CharField(max_length=255)
+    pays = models.CharField(max_length=255)
+    ville = models.CharField(max_length=255)
+    adresse = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    adresse = models.TextField()
+    nom_client = models.CharField(max_length=255)
     livraison = models.ForeignKey(Livraison,on_delete=models.SET_NULL,null=True)
+    prix_total = models.FloatField()
+    
+    @property
+    def prix_total(self):
+        produits = ProduitCommande.objects.filter(commande__id=self.id)
+        somme = 0
+        for produit in produits:
+            somme += produit.prix_total()
+        return somme
     
     def _str_(self):
         return str(self.statut_commande)
 
 
-class Produit(models.Model):
-    designation_produit = models.CharField(max_length=255)
-    prix_produit = models.DecimalField(max_digits=10, decimal_places=2)
-    details_produit = models.TextField()
-    image_produit = models.ImageField(upload_to='produits/')
-    qte_produit = models.PositiveIntegerField()
-    categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE)
-    commande= models.ForeignKey(Commande,on_delete=models.SET_NULL,null=True)
+class ProduitPanier(models.Model):
+    class Meta:
+        verbose_name="Produits Par Panier"
+        verbose_name_plural="Produits Par Panier"
 
-
+    qte_produit = models.PositiveIntegerField(default=1)
+    panier = models.ForeignKey(Panier, on_delete=models.CASCADE, related_name="produits")
+    produit = models.ForeignKey(Produit, on_delete=models.SET_NULL, null=True)
+    
+    @property
+    def prix_total(self):
+        return self.qte_produit*self.produit.prix_produit
+    
     def _str_(self):
-        return self.designation_produit
+        return self.produit.designation_produit
 
 
+class ProduitCommande(models.Model):
+    class Meta:
+        verbose_name="Produits Par Panier"
+        verbose_name_plural="Produits Par Panier"
 
+    qte_produit = models.PositiveIntegerField(default=1)
+    commande = models.ForeignKey(Commande, on_delete=models.CASCADE)
+    produit = models.ForeignKey(Produit, on_delete=models.SET_NULL, null=True)
 
-
+    @property
+    def prix_total(self):
+        self.prix = self.qte_produit*Produit.prix_produit
+        return self.qte_produit*Produit.prix_produit
+    
+    def _str_(self):
+        return self.produit.designation_produit
+    
 
 class Operateur(models.Model):
    
